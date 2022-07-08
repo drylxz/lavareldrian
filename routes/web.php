689a3e9befb1798374cd91ed7594ecf2fe4e9736
grizz12 <raka_088@smkassalaambandung.sch.id>
2022-07-08 11:34:47 +0700

    Belajar Laravel

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,31 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// route basic
+Route::get('about', function () {
+    return  view('Hallo About');
+});
+
+Route::get('profile', function () {
+    $nama = "Adrian";
+    $umur = "18";
+    $alamat = "Bandung";
+    $bendera = "Jerman";
+    $hobi = "Tidur";
+    return view('pages/profile' , compact('nama','umur','alamat','bendera','hobi'));
+});
+
+// route parameter
+Route::get('biodata/{nama}' , function ($a) {
+    return view('pages/biodata' , compact('a'));
+}) ;
+
+Route::get('makan/{makan}/{minum}/{harga}' , function ($makan,$minum,$harga) {
+    return view('pages/makan' , compact('makan','minum','harga'));
+}) ;
+
+// route optional parameter
+Route::get('pesan/{nama?}' , function ($a = "null") {
+    return view('pages/pesan' , compact('a'));
+}) ;
